0405b6681ee674cb36d09adf0800154187298ee0
unknown <fmuntada>
2021-10-18 14:41:26 +0200

    wip

--- packages/arquetipo-storybook/.storybook/main.js
+++ packages/arquetipo-storybook/.storybook/main.js
@@ -4,7 +4,6 @@ module.exports = {
         "@storybook/addon-actions",
         "@storybook/addon-links",
         "@storybook/addon-notes",
-        "@storybook/addon-knobs",
         '@storybook/addon-docs'
     ],
     core: {
